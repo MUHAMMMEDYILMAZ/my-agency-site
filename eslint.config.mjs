a0c5122bf303d41c6ea96b07ed9d75e4b20c5e32
MUHAMMMEDYILMAZ <mohammad.jamous@ogr.sakarya.edu.tr>
2025-12-13 22:27:17 +0300

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 import nextTs from "eslint-config-next/typescript.js";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  globalIgnores([
+export default defineConfig({
+  overrides: [
+    ...(nextVitals.overrides || []),
+    ...(nextTs.overrides || []),
+  ],
+  ignorePatterns: [
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+  ],
+});
